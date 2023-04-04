5342a79de754d0fabee79d6662a9a21f2ae5ca67
Jaime Florian <jaimeflorian27@gmail.com>
2023-04-03 23:59:20 -0700

    feat: first commit, setting up CLI basics

---
+++ rez_venv/main.py
@@ -0,0 +1,41 @@
+import argparse
+import os
+import subprocess
+import sys
+import venv
+
+
+def create_venv(venv_path, system_site_packages):
+    """Create a new virtual environment."""
+    builder = venv.EnvBuilder(system_site_packages=system_site_packages)
+    builder.create(venv_path)
+
+
+def parse_args(args):
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description="Create a virtual environment")
+    parser.add_argument(
+        "venv_path",
+        type=str,
+        nargs='?',
+        default=os.path.join(os.getcwd(), "venv"),
+        help="Path to the virtual environment",
+    )
+
+    parser.add_argument(
+        "--system-site-packages",
+        type=bool,
+        nargs='?',
+        default=True,
+        help="Give access to the system site-packages",
+    )
+    return parser.parse_args(args)
+
+
+def main():
+    args = parse_args(sys.argv[1:])
+    create_venv(args.venv_path, args.system_site_packages)
+
+
+if __name__ == "__main__":
+    main()
